84d2b993c5f3396434b4e5e473cbb07c081ad44e
Nomsku <albin.sihvola@metropolia.fi>
2022-11-05 22:35:20 +0200

    middleware

--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-const {user_list_get, user_get} = require('../controllers/userController');
+const {user_list_get, user_get, user_post} = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', user_list_get);
@@ -10,9 +10,7 @@ router.get('/', user_list_get);
   router.get('/:id', user_get);
 
   
-  router.post('/', (req, res) => {
-    res.send('From this endpoint you can add users.')
-  });
+  router.post('/', user_post);
   
   
   router.put('/', (req, res) => {
