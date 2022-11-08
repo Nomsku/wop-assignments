1d9fa6e86039ceabe5e52f74a178cfb162f1218c
Nomsku <albin.sihvola@metropolia.fi>
2022-11-08 19:02:32 +0200

    grouping

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -6,19 +6,20 @@ const {cat_list_get, cat_get, cat_post, cat_update_put, cat_delete} = require('.
 
 const router = express.Router();
 
-router.get('/', cat_list_get);
-    
+router.route('/').
+  get(cat_list_get).
+  post(upload.single('cat'), cat_post).
+  put(cat_update_put);
   
   
-  router.get('/:id', cat_get);
 
-  
-  router.post('/', upload.single('cat'), cat_post);
-  
-  
-  router.put('/', cat_update_put);
+router.route('/:id').get(cat_get).delete(cat_delete);
   
   
-  router.delete('/:id', cat_delete);
+  
+  
+  
+  
+  
 
   module.exports = router;
